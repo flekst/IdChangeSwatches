0b33eae7df52d2678f10daba4a418c553927e684
Eugeny Borisov <flekst@gmail.com>
2014-10-17 15:36:15 +0400

    Initial Release

---
+++ changeSwatches.jsx
@@ -0,0 +1,21 @@
+﻿/* (c) Eugeny Borisov 2014 */
+#target Indesign
+try { var mySwatches = app.activeDocument.swatches;     } catch(err) { exit(); }
+
+var swapData = [
+  pswd("testSwatch", 1,2,3,4),
+  pswd("testSwatch2", 1,2,3,4)
+];
+
+/*PrepareSWapData*/
+function pswd(nameVal, c,m,y,k) {
+  var swapData= {name: nameVal, cmyk: [c,m,y,k] };
+  return swapData;
+  }
+
+var i = swapData.length;
+while (i--) {
+    try {
+            mySwatches.itemByName(swapData[i].name).colorValue = swapData[i].cmyk;
+    } catch(err){}
+ }
